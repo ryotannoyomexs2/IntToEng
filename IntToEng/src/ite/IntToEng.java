025dd5bc39030520411c3de3b8d133c1c1ee4112
ishidanozomi <ishidanozomi@172.19.12.54>
2018-05-17 14:47:27 +0900

    文字を追加しました。

--- IntToEng/src/ite/IntToEng.java
+++ IntToEng/src/ite/IntToEng.java
@@ -5,51 +5,28 @@ import java.util.Scanner;
 
 
 public class IntToEng {
-
 public static void main(String[] args) {
-
-
-
+		System.out.println("0から100までの数字を入力して下さい。");
+		System.out.print("入力：");
         Scanner sc = new Scanner(System.in);
-
         int input = sc.nextInt();
-
-
-
-        System.out.println(translateEng(input));
-
-
-
+        System.out.println("出力："+translateEng(input));
     }
-
-
 
     // 数値を英訳する変換するメソッド
-
     static String translateEng(int n) {
-
     String[] num1 = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; //1桁
-
     String[] num2 = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixtenn", "seventeen", "eighteen", "nineteen"}; //10代
-
     String[] num3 = {"ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety","onehundred"}; //10で割り切れるum
-
     String num = "";
-
+    
     if(n<9) return num1[n];
-
     if(10<n && n<20) return num2[n-10-1];
-
     if(n%10==0) return num3[n/10-1];
-
     else {
-
     num = num3[n/10-1] + num1[n%10];
-
     return num;
-
     }
-
     }
 
 }
